742bdba6ec18f5a76ba3a7ef8b62937cb4065e66
LongNTDev <codeinlife10@example.com>
2023-10-23 06:51:39 +0700

    ok

---
+++ SS7/index.js
@@ -0,0 +1,69 @@
+// Câu lệnh điều kiện, câu lệnh rẽ nhánh
+// ==, ===, >, <, &&, ||  -> Biểu thức và giá trị Boolean
+// if (false) {
+//     // Khối lệnh thực hiện nếu như điều kiện xảy ra
+//     console.log("Khối lệnh này đã được thực hiện")
+// }
+
+// let a = +prompt("Hãy nhập vào 1 số để kiểm tra")
+
+// if(a > 0) {
+//     console.log(`${a} là số dương`)
+// } else if(a < 0) {
+//     console.log(`${a} là số âm`)
+// } else if(a == 0) {
+//     console.log(`Đây là số 0`)
+// } else { // Tất cả mọi trường hợp còn lại 
+//     console.log("Giá trị bạn vừa nhập không phải 1 số")
+// }
+
+// let a = 5
+// let b = 10
+
+// if(a > 5 && b > 5) {
+//     console.log("Chạy vào đây rồi nè")
+// }
+
+// let today = "Monday"
+// switch(today) {
+//     case "Monday": 
+//         console.log("Hôm nay là thứ hai")
+//         break
+//     case "Tuesday": 
+//         console.log("Hôm nay là thứ ba")
+//         break
+// }
+
+// Vòng lặp 
+// for(Giá trị khởi tạo; Điều kiện tiếp tục; Bước nhảy ) {
+
+// }
+
+
+// for(let i = 0; i < 10; i++) {
+//     // Khối code được thực hiện
+   
+//     for(let j = 0; j < 10; j++) {
+//     // Khối code được thực hiện
+   
+//     }
+// }
+
+// for(let i = 0; i < 10; i++) {
+//     if(i % 2 != 0) {
+//         continue
+//     }
+//     console.log(i)
+
+// }
+
+// let isStr = true
+
+// while(true) {
+//     console.log("Trước khi break")
+//     if(isStr) {
+//         break
+//     } 
+//     console.log("Sau khi break");
+// }
+ 
